f8d172cfed04267afe531add2b17ea719b5ed3ec
Shubham Panchal <panchalshubhamdev@gmail.com>
2025-03-22 09:22:42 +0530

    build: update versionCode and versionName to '6'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.shubham0204.smollmandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
