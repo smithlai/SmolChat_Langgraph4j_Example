8c22801a0a3208b7521781c413be10390bf66583
Shubham Panchal <panchalshubhamdev@gmail.com>
2025-03-30 11:17:06 +0530

    build: strip symbols from libggufreader.so and set minify = true in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("release")
         }
