ce494748ea01f8b6f10fb1d798fcd3c2a1e19456
Shubham Panchal <panchalshubhamdev@gmail.com>
2025-05-03 19:07:00 +0530

    Release v7 changes for FDroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.shubham0204.smollmandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "6"
+        versionCode = 7
+        versionName = "7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
